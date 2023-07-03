4de290b5e320353928cc4d1b94293f9cec79813c
Milhan Osman <milhanomarosman@gmail.com>
2023-07-03 13:26:38 +0300

    modifying some functionality

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,15 +1,5 @@
 class HeroPowersController < ApplicationController
   protect_from_forgery with: :null_session
   rescue_from ActiveRecord::RecordInvalid, with: :invalid_heropower_params
- def create
-  heropower = HeroPower.create!(heropower_params)
-  hero = Hero.find(heropower_params[:hero_id])
-  render json: hero,serializer: HerowithpowerSerializer, status: :created
- end
- def heropower_params
-  params.permit(:hero_id, :power_id, :strength)
- end
- def invalid_heropower_params(invalid)
-  render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
- end
+
 end
