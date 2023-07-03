7029fe3f24cf596e683fa86fa818f95c4adf4233
Milhan Osman <milhanomarosman@gmail.com>
2023-07-03 13:18:11 +0300

    URL mapping

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+ resources :hero_powers, only: [:create]
+ resources :powers, only: [:index, :show, :update]
+  # resources :heros, only: [:index, :show]
+
+
+  # get '/hero_powers', to:'hero_powers#create'
+  get '/heroes', to: 'heros#index'
+  get '/heroes/:id', to: 'heros#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
